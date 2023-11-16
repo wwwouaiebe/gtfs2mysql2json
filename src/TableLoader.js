018c17107da9f61a4080a38879d02d2ce2e10260
wwwouaiebe <github.christian@guyette.be>
2023-11-16 17:17:39 +0100

    Build continued...

--- src/TableLoader.js
+++ src/TableLoader.js
@@ -1,10 +1,36 @@
+/*
+Copyright - 2023 - wwwouaiebe - Contact: https://www.ouaie.be/
+
+This  program is free software;
+you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation;
+either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/*
+Changes:
+	- v1.0.0:
+		- created
+*/
+/* ------------------------------------------------------------------------------------------------------------------------- */
+
 import theConfig from './Config.js';
 import fs from 'fs';
 import theMySqlDb from './MySqlDb.js';
 
+/* ------------------------------------------------------------------------------------------------------------------------- */
 /**
  * Coming soon...
  */
+/* ------------------------------------------------------------------------------------------------------------------------- */
 
 class TableLoader {
 
@@ -67,3 +93,5 @@ class TableLoader {
 }
 
 export default TableLoader;
+
+/* --- End of file --------------------------------------------------------------------------------------------------------- */
